1f7eef42a8f05e9a60b9db5e485e62ab3d669000
abedmir31 <54549012+AbedMir31@users.noreply.github.com>
2021-12-12 23:39:35 -0600

    Add nav bar

--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -2,19 +2,36 @@ import React from "react";
 
 export function Home() {
   return (
-    <section className="wrapper">
-        <div className="content">
-            <header>
-                <h1>How Much is Your League of Legends Account Worth?</h1>
-            </header>
-            <section>
-                <p>Enter your Summoner Name to find out how much your skins are worth:</p>
-            </section>
-            <footer>
-                <input type="text" placeholder="Name.."></input>
-                <button>GO</button>
-            </footer>
-        </div>
-    </section>
+    <div>
+        <header className="topbar">
+            <a href="./" text>LoL Skin Appraiser</a>
+        </header>
+        <section className="wrapper">
+            <div className="content">
+                <header>
+                    <h1>How Much is Your League of Legends Account Worth?</h1>
+                </header>
+                <section>
+                    <p>Enter your Summoner Name to find out how much your skins are worth:</p>
+                </section>
+                <footer>
+                    <input type="text" placeholder="Name.."></input>
+                    <button>GO</button>
+                </footer>
+            </div>
+        </section>
+        <section className="wrapper">
+            <div className="disclaimer">
+                <section>
+                    <p>
+                        isn't endorsed by Riot Games and doesn't reflect the
+                        views or opinions of Riot Games or anyone officially involved in
+                        producing or managing League of Legends. League of Legends and Riot
+                        Games are trademarks or registered trademarks of Riot Games, Inc.
+                    </p>
+                </section>
+            </div>
+        </section>
+    </div>
   );
 }
